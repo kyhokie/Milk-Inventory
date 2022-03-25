df90ad07c3f7c1ceb75853e7aeffbd9a2ffcab8e
kyhokie <kyhokie@gmail.com>
2022-03-24 21:48:30 -0400

    Whitespace clean-up

--- Milk-Inventory-2Tests/MilkInventoryTests.cs
+++ Milk-Inventory-2Tests/MilkInventoryTests.cs
@@ -1,10 +1,4 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using MilkInventory2;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MilkInventory2.Tests
 {
